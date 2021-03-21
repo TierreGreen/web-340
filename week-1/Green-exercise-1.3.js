50cbb7f728da5b394932f6ef7e6f29b1f99629d8
Tiérre Green <tigreen@my365.bellevue.edu>
2021-03-21 17:03:58 -0400

    updated 1.3 with comments that I forgot to add.

--- week-1/Green-exercise-1.3.js
+++ week-1/Green-exercise-1.3.js
@@ -1,7 +1,18 @@
+/**
+ * Title: Exercise 1.3
+ * Author: Professor Richard Krasso
+ * Date: March 21, 20221
+ * Modified by: Tierre Green
+ * Description: node file parsed URL
+ */
+
+//url required code
 var url = require('url');
 
+//url to be parsed and debugged
 var parsedURL = url.parse('https://tierregreen.github.io/web-231/?name=green');
 
+//parsed URL console.log protocol host and query
 console.log(parsedURL.protocol);
 console.log(parsedURL.host);
 console.log(parsedURL.query);
