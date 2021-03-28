a1b69ea771394795b89cd6619618b235a39f85e0
Tiérre Green <tigreen@my365.bellevue.edu>
2021-03-28 14:13:33 -0400

    updated comments

--- week-2/hello-world/Green-exercise-2.2.js
+++ week-2/hello-world/Green-exercise-2.2.js
@@ -1,14 +1,27 @@
+/**
+ * Title: Green-exercise-2.2
+ * Author: Professor Richard Krasso
+ * Modified by: Tierre Green
+ * Date: March 28, 2021
+ * description: hello world express app.
+ */
+
+
+//required express module
 var express = require('express');
 var http = require('http');
 
+//express function called  to start express
 var app = express();
 
+//middle ware passing function through request handler
 app.use(function(req, res) {
     console.log('In comes a request to: %s', req.url);
 
     res.end('Hello World\n');
 })
 
+//starts server and console log to verify response
 http.createServer(app).listen(8080, function() {
     console.log('Application started on port %s', 8080);
 });
