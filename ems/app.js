2887e78d3cf20153796477e1cba1e766f1b4a5ff
Tiérre Green <tigreen@my365.bellevue.edu>
2021-04-30 12:50:10 -0400

    adding additional files.

---
+++ ems/app.js
@@ -0,0 +1,39 @@
+/**
+ * Title: App.js file for Milestone
+ * Author: Professor Krasso
+ * Modified by: Tierre Green
+ * Description: Milestone App.js file
+ */
+
+
+var express = require("express");
+
+var http = require("http");
+
+var path = require("path");
+
+var logger = require("morgan");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(logger("short"));
+
+app.get("/", function (request, response) {
+
+    response.render("index", {
+
+        title: "Home page"
+
+    });
+
+});
+
+http.createServer(app).listen(8080, function() {
+
+    console.log("Application started on port 8080!");
+
+});
