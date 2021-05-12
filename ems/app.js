34d347053336ad138443547a3f6ddbb98fc89fe3
Tiérre Green <tigreen@my365.bellevue.edu>
2021-05-12 06:45:33 -0400

    added files

--- ems/app.js
+++ ems/app.js
@@ -16,6 +16,15 @@ var logger = require("morgan");
 
 var helmet = require("helmet");
 
+var bodyParser = require("body-parser");
+
+var cookieParser = require("cookie-parser");
+
+var csrf = require("csurf");
+
+// setup csrf protection
+var csrfProtection = csrf({cookie: true});
+
 var app = express();
 
 const mongoose = require('mongoose');
@@ -54,32 +63,128 @@ app.use(logger("short"));
 
 app.use(helmet.xssFilter());
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(cookieParser());
+
+app.use(csrfProtection);
+
+app.use(function(req, res, next) {
+
+    var token = req.csrfToken();
+
+    res.cookie('XSRF-TOKEN', token);
+
+    res.locals.csrfToken = token;
+
+    next();
+});
+
+//get homepage
 app.get("/", function(req, res){
 
     res.render("index", {
 
-        message: "XSS Prevention Example"
-    });
-
-});
-
-app.get("/", function (req, res) {
-
-    res.render("index", {
+        message: "XSS Prevention Example",
 
         title: "Home page"
-
     });
 
 });
 
-//get the contact file. 
+//about file
+app.get('/about', function(req, res){
+    
+    res.render("About", {
+        title: "About"
+    });
+});
+
+//contact file
+app.get('/contact', function(req, res){
+
+    res.render('contact', {
+        title: "Contact Us"
+    });
+});
+
+//get the list file. 
 app.get('/list', function(req, res){
+
     res.render('list',{
+
         title: "Employee Records"
     });
-}); 
+});
 
+//new file. 
+app.get('/new', function(req, res){
+
+    res.render('new',{
+
+        title: "New",
+
+        message: "Please enter your first and last name..."
+    });
+});
+
+//Form for persons first and last name. 
+app.post("/process", function(req, res) {
+
+    console.log(req.body.txtName);
+
+    if (!req.body.firstName) {
+
+        res.status(400).send("You must enter a first name.");
+
+        return; 
+    }
+
+    if (!req.body.lastName) {
+
+        res.status(400).send("You must enter a last name.");
+
+        return; 
+    }
+
+    var firstName = req.body.firstName;
+
+    var lastName = req.body.lastName;
+
+    var employee = new Employee({
+
+        firstName: firstName,
+
+        lastName: lastName
+    });
+
+    employee.save(function (error) {
+
+        if (error) throw error;
+
+        console.log(firstName + lastName + " your entry is saved!")
+    });
+    res.redirect("/");
+
+});
+
+//get list page. 
+app.get("/list", function(req, res) {
+
+    Employee.find({}, function(error, employees) {
+
+        if (error) throw error;
+
+        res.render("list", {
+
+            title: "Employee List",
+
+            employees: employees
+        });
+    });
+});
 
 http.createServer(app).listen(3001, function() {
 
